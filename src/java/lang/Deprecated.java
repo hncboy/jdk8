a09bf0d8e14966446e3e10c62f65880a46a66b87
hncboy <619452863@qq.com>
2019-09-04 13:33:54 +0800

    Deprecated

--- src/java/lang/Deprecated.java
+++ src/java/lang/Deprecated.java
@@ -29,6 +29,8 @@ import java.lang.annotation.*;
 import static java.lang.annotation.ElementType.*;
 
 /**
+ * 不推荐程序员使用 Deprecated 注解的代码，因为一般都是危险的或者有更好的可替代的存在。
+ * 当我们使用 deprecated 的代码或者 override 后类型不为 deprecated 的代码，编译器都会警告。
  * A program element annotated &#64;Deprecated is one that programmers
  * are discouraged from using, typically because it is dangerous,
  * or because a better alternative exists.  Compilers warn when a
