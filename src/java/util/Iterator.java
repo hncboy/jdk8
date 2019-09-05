1da6c024b2d5c32f496b13051fbc2bc364872b09
hncboy <619452863@qq.com>
2019-09-05 14:53:02 +0800

    Iterator

--- src/java/util/Iterator.java
+++ src/java/util/Iterator.java
@@ -52,7 +52,9 @@ import java.util.function.Consumer;
  * @since 1.2
  */
 public interface Iterator<E> {
-    /**
+    /**++
+     * 如果迭代器有更多元素的话就返回
+     * （换句话说，如果 next 函数返回一个元素而不是抛出异常则返回 true）
      * Returns {@code true} if the iteration has more elements.
      * (In other words, returns {@code true} if {@link #next} would
      * return an element rather than throwing an exception.)
@@ -61,7 +63,8 @@ public interface Iterator<E> {
      */
     boolean hasNext();
 
-    /**
+    /**++
+     * 返回迭代的下一个元素，如果没有下一个则抛出 NoSuchElementException
      * Returns the next element in the iteration.
      *
      * @return the next element in the iteration
@@ -69,7 +72,11 @@ public interface Iterator<E> {
      */
     E next();
 
-    /**
+    /**++
+     * 从底层集合中删除此迭代器的最后一个元素（可选操作）
+     * next 方法每次只能调用一次
+     * 当迭代器通过任何方式进行但不是调用这个方法，这个迭代器行为对底层
+     * 集合的操作是不明确的。
      * Removes from the underlying collection the last element returned
      * by this iterator (optional operation).  This method can be called
      * only once per call to {@link #next}.  The behavior of an iterator
@@ -78,12 +85,16 @@ public interface Iterator<E> {
      * method.
      *
      * @implSpec
+     * 默认实现方法会抛出 UnsupportedOperationException 并且不执行其它操作
      * The default implementation throws an instance of
      * {@link UnsupportedOperationException} and performs no other action.
      *
+     * 如果该迭代器不支持 remove 也会抛出 UnsupportedOperationException
      * @throws UnsupportedOperationException if the {@code remove}
      *         operation is not supported by this iterator
      *
+     * 如果没有调用 next 方法或者在最后一次调用 next 方法的时候调用 remove 方法
+     * 会抛出 IllegalStateException
      * @throws IllegalStateException if the {@code next} method has not
      *         yet been called, or the {@code remove} method has already
      *         been called after the last call to the {@code next}
@@ -93,13 +104,16 @@ public interface Iterator<E> {
         throw new UnsupportedOperationException("remove");
     }
 
-    /**
+    /**++
+     * 对每个剩余元素执行给定操作，直到处理完所有元素或操作引发异常。
+     * 如果指定了顺序，则按指定的迭代器顺序执行。
+     * 操作抛出的异常会转发给调用者。
      * Performs the given action for each remaining element until all elements
      * have been processed or the action throws an exception.  Actions are
      * performed in the order of iteration, if that order is specified.
      * Exceptions thrown by the action are relayed to the caller.
      *
-     * @implSpec
+     * @implSpec 实现规范 implement specification
      * <p>The default implementation behaves as if:
      * <pre>{@code
      *     while (hasNext())
